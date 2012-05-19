d39162c71b1ffe3b8518a426eb21e347dca13c53
Risugami <Risugami>
2012-05-19 15:57:39 -0500

    adding flags

--- modulesrc/ModuleRegexReplace.java
+++ modulesrc/ModuleRegexReplace.java
@@ -24,11 +24,26 @@ public class ModuleRegexReplace extends Module {
 		String s = event.getMessage();
 		if (!s.startsWith("s/")) return;
 		String[] args = s.split("/");
-		if (args.length != 3) return;
+		if (args.length < 3 || args.length > 4) return;
 		if (args[1].isEmpty()) return;
+		int flags = 0;
+		boolean single = true;
+		if (args.length == 4 && !args[3].isEmpty()) {
+			for (char c : args[3].toCharArray()) {
+				switch (c) {
+				case 'd': flags |= Pattern.UNIX_LINES; break;
+				case 'g': single = false; break;
+				case 'i': flags |= Pattern.CASE_INSENSITIVE; break;
+				case 'm': flags |= Pattern.MULTILINE; break;
+				case 's': flags |= Pattern.DOTALL; break;
+				case 'u': flags |= Pattern.UNICODE_CASE; break;
+				case 'x': flags |= Pattern.COMMENTS; break;
+				}
+			}
+		}
 		Module module = Module.getModule("rollback");
 		if (module == null) return;
-		Pattern pattern = Pattern.compile(args[1]);
+		Pattern pattern = Pattern.compile(args[1],flags);
 		Matcher matcher = pattern.matcher("");
 		Method method = module.getClass().getDeclaredMethod("getRollbackLines", Class.class, String.class, String.class, String.class, boolean.class, int.class, int.class);
 		@SuppressWarnings("unchecked")
@@ -38,7 +53,7 @@ public class ModuleRegexReplace extends Module {
 		for (int i = lines.size()-2; i>=0 && !found; i--) {
 			LineMessage line = lines.get(i);
 			matcher.reset(line.text);
-			while (matcher.find()) {
+			while (matcher.find() && !(single && found)) {
 				found = true;
 				matcher.appendReplacement(sb, args[2]);
 			}
